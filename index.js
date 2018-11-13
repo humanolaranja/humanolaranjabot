a5ed88af808bd2411d25bfb4ce62e7519600a8d1
Humano Laranja <contato.carlos@outlook.com>
2018-11-13 16:50:53 -0200

    hears /xyz, getItem

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const env = (process.env.NODE_ENV !== 'production') ? require('dotenv').config().parsed : process.env;
-console.log(env);
 const http = require('http');
 const port = (process.env.PORT || 5000);
 const Telegraf = require('telegraf');
@@ -22,22 +21,62 @@ http.createServer((req, res) => {
   res.end();
 }).listen(port);
 
+const getTitle = (id) => {
+  let temp = comidas.filter((comida) => comida.id == id);
+  return (temp.length > 0) ? temp[0].title : "";
+}
+
+const getItem = (id) => {
+  let item = comidas.filter((comida) => comida.id == id);
+  if(item.length > 0) {
+    item = item[0];
+  }
+  else {
+    return "Desculpe, esse item não existe";
+  }
+  let text = `*${item.title}*`;
+  text += (item.description) ? ` - ${item.description}\n` : '\n';
+  if(item.alternatives.length > 0) {
+    text += `\nAlternativas:\n`;
+    for (let i = 0; i < item.alternatives.length; i++) {
+      let altname = getTitle(item.alternatives[i]);
+      text += `/${item.alternatives[i]} - ${altname}\n`;
+    }
+  }
+  return text;
+}
+
 bot.start(async (ctx) => {
   const from = ctx.update.message.from;
   if(from.id === 169949340) {
     await ctx.reply(`Seja bem vindo,  ${from.first_name} ${from.last_name}!`);
     await ctx.reply(`Qual refeição você gostaria de verificar?`, tecladoStart);
-    console.log(comidas);
   }
   else {
     await ctx.reply(`Desculpe, mas eu fui feito apenas para o @HumanoLaranja`);
   }
 });
 
+//hears
 bot.hears(/foco/i, (ctx) => ctx.reply('Você consegue! 💪'));
 
-bot.command('about', async ctx => await ctx.reply('Criado por Humano Laranja - http://github.com/humanolaranja/'));
+bot.hears('☕ Desjejum', (ctx) => {
+  let desjejum = comidas.filter((comida) => (comida.when == ctx.match && comida.hide == 0));
+    desjejum.forEach(async (item) => {
+      await ctx.replyWithMarkdown(getItem(item.id));
+    });
+});
 
-// bot.hears(['Suco', 'Refrigerante'], (ctx) => ctx.reply(`Você respondeu: ${ctx.match}`))
+bot.hears('🍳 Café da manhã', (ctx) => {
+});
+
+bot.hears(/\/\d{3}/i, async ctx => {
+  let id = ctx.match[0].substr(1);
+  await ctx.replyWithMarkdown(getItem(id));
+});
+
+
+//commands
+bot.command('about', async ctx => await ctx.reply('Criado por Humano Laranja - http://github.com/humanolaranja/'));
 
 bot.startPolling();
